b12a8b2a5dc68e8fc6b84e7b999aa5d434164ede
Kushal-Chandar <kushalchandariit@gmail.com>
2023-07-22 20:28:44 +0530

    project v0.0.1 complete

---
+++ app/page.jsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { AiFillGithub } from "react-icons/ai";
+import RealtimeDetection from "../components/realtimedetection";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [camera, setCamera] = useState(true);
+  const [notice, setNotice] = useState(true);
+  const [gestureInfo, setGestureInfo] = useState(
+    "**Click to get a description of each symbol**"
+  );
+
+  useEffect(() => {
+    navigator.permissions.query({ name: "camera" }).then(function (result) {
+      if (!(result.state === "granted")) {
+        setCamera(false);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl m-5">Sign Language Translator</h1>
+      <p>
+        I have used transfer learning on a pre trained object detection model to
+        recognize the following 5 gestures.
+      </p>
+      <div className="flex flex-row justify-center lg:text-7xl text-5xl m-5">
+        <button
+          className="shadow-2xl hover:bg-neutral-900 rounded-lg"
+          onClick={() => setGestureInfo("Hello: Hold a steady hand.")}
+        >
+          👋
+        </button>
+        <button
+          className="shadow-2xl hover:bg-neutral-900 rounded-lg"
+          onClick={() => setGestureInfo("I Love You")}
+        >
+          🤟
+        </button>
+        <button
+          className="shadow-2xl hover:bg-neutral-900 rounded-lg"
+          onClick={() =>
+            setGestureInfo("No: Pinch your hand but leave a little gap.")
+          }
+        >
+          🤏
+        </button>
+        <button
+          className="shadow-2xl hover:bg-neutral-900 rounded-lg"
+          onClick={() => setGestureInfo("Yes: Make a fist.")}
+        >
+          👊
+        </button>
+        <button
+          className="shadow-2xl hover:bg-neutral-900 rounded-lg flex flex-row"
+          onClick={() =>
+            setGestureInfo("Thank You: Hold Both hands close to your chin.")
+          }
+        >
+          <div className="-mr-2">🤚</div>
+          <div className="scale-x-[-1] -ml-2">🤚</div>
+        </button>
+      </div>
+      <p className="text-xs -mt-2">{gestureInfo}</p>
+      <div className="flex flex-col items-center">
+        {notice && (
+          <button
+            className="max-w-md mt-2 text-sm w-full text-neutral-800 bg-neutral-200 shadow-md rounded-lg p-2 flex flex-col justify-normal items-center gap-y-3 transition"
+            onClick={() => {
+              setNotice(false);
+            }}
+          >
+            <p>
+              Depending on your internet connection and your computer
+              specifications, it might take a while to load the modal.
+            </p>
+            <p>
+              If the demo is too slow you can try enabling hardware acceleration
+              on browser.
+            </p>
+            <p className="text-xs hover:text-neutral-400">*Touch to close*</p>
+          </button>
+        )}
+        {!camera ? (
+          <button
+            className="rounded my-5 mx-auto border bg-neutral-200 text-neutral-800 p-1"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Please allow permission to camera
+          </button>
+        ) : (
+          <RealtimeDetection />
+        )}
+        <a
+          href="https://github.com/Kushal-Chandar/Sign-Language-Translator"
+          target="_blank"
+        >
+          <AiFillGithub
+            size={30}
+            className="mx-auto mt-2 hover:text-black transition"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/realtimedetection.jsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+
+const labelMap = {
+  1: { name: "Hello", color: "red" },
+  2: { name: "Yes", color: "blue" },
+  3: { name: "No", color: "purple" },
+  4: { name: "Thank You", color: "yellow" },
+  5: { name: "I Love You", color: "lime" },
+};
+
+export const drawRect = (
+  boxes,
+  classes,
+  scores,
+  threshold,
+  imgWidth,
+  imgHeight,
+  ctx
+) => {
+  for (let i = 0; i < boxes.length; i++) {
+    if (boxes[i] && classes[i] && scores[i] > threshold) {
+      const [y, x, height, width] = boxes[i];
+      const text = classes[i];
+
+      ctx.strokeStyle = labelMap[text]["color"];
+      ctx.lineWidth = 5;
+      ctx.fillStyle = "black";
+      ctx.font = "30px Arial";
+
+      ctx.rect(
+        x * imgWidth,
+        y * imgHeight,
+        (width * imgWidth) / 2,
+        (height * imgHeight) / 1.5
+      );
+      ctx.stroke();
+      ctx.scale(-1, 1);
+      ctx.fillText(
+        labelMap[text]["name"] + " - " + Math.round(scores[i] * 100) / 100,
+        -(x * imgWidth) - (width * imgHeight) / 1.5,
+        y * imgHeight - 10
+      );
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
+  }
+};
+
+function RealtimeDetection() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const runCoco = async () => {
+      const net = await tf.loadGraphModel("/model/model.json");
+      setInterval(() => {
+        detect(net);
+      }, 30);
+    };
+    runCoco();
+  }, []);
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast("int32");
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
+
+      const boxes = await obj[1].array();
+      const classes = await obj[2].array();
+      const scores = await obj[4].array();
+
+      const ctx = canvasRef.current.getContext("2d");
+
+      requestAnimationFrame(() => {
+        drawRect(
+          boxes[0],
+          classes[0],
+          scores[0],
+          0.8,
+          videoWidth,
+          videoHeight,
+          ctx
+        );
+      });
+
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
+    }
+  };
+  return (
+    <div className="max-w-[640px] max-h-[480px] w-full h-fit my-10 scale-x-[-1]">
+      <Webcam
+        className="absolute border rounded-lg mx-auto left-0 right-0 z-10 h-fit w-full"
+        ref={webcamRef}
+        muted={true}
+        alt="Camera is Disabled"
+      />
+      <canvas
+        ref={canvasRef}
+        className="relative border rounded-lg mx-auto left-0 right-0 z-20 h-fit w-full"
+      />
+    </div>
+  );
+}
+
+export default RealtimeDetection;
